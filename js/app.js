39cb797eb90ed2bf4a172aad9f26b22b1f3b94e3
Alex Le <alexvanle1998@gmail.com>
2020-01-31 11:31:30 -0600

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// var options = document.getElementById('options');
+// var startBtn = document.getElementsByClassName('startBtn');
+// let startBtn = document.getElementById('startBtn')
+// let 
+// startBtn.addEventListener('click', startGame)
+
+// function startGame() {
+//     // options.style.display = 'none';
+//     // startBtn.style.display = 'none';
+//     console.log('Started');
+//     startBtn.classList.add('hide')
+// }
